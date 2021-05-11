743a4e12de01556a48794e61e9cbb17f2a9bf20f
nwuking <2283300457@qq.com>
2021-05-11 19:15:42 +0800

    You will find the love you need, when you learn to love youserlf!

--- netlib/TimeZone.cc
+++ netlib/TimeZone.cc
@@ -2,6 +2,12 @@
 
 #include <vector>
 #include <string>
+#include <algorithm>
+
+#include <stdio.h>
+#include <strings.h>
+#include <string.h>
+#include <assert.h>
 
 namespace netlib
 {
@@ -55,6 +61,65 @@ struct LocalTime
 
 const int cSecondsPerDay = 24 * 60 * 60;
 
+class File
+{
+public:
+    File(const char *zonefile)
+        : _fp(::fopen(zonefile, "rb"))
+    {
+    }
+
+    ~File() {
+        if(_fp) {
+            ::fclose(_fp);
+        }
+    }
+
+    bool vaild() const {
+        return _fp;
+    }
+
+
+
+private:
+    FILE *_fp;
+};
+
+bool readTimeZoneFile(const char *zonefile, struct TimeZone::Data *data) {
+    //
+    File f(zonefile);
+
+    if(f.vaild()) {
+
+    }
+}
+
+const LocalTime* findLocalTime(const TimeZone::Data &data, transition sentry, Comp comp) {
+    const LocalTime *local = NULL;
+
+    if(data.transitions.empty() || comp(sentry, data.transitions.front())) {
+        local = &data.localtimes.front();
+    }
+    else {
+        std::vector<transition>::const_iterator transI = std::lower_bound(data.transitions.begin(),
+                                                                          data.transitions.end(),
+                                                                          sentry,
+                                                                          comp);
+        if(transI != data.transitions.end()) {
+            if(!comp(sentry, *transI)) {
+                assert(transI != data.transitions.begin());
+                --transI;
+            }
+            local = &data.localtimes[transI->localtimeIdx];
+        }
+        else {
+            local = &data.localtimes[data.transitions.back().localtimeIdx];
+        }
+    }
+
+    return local;
+}
+
 }
 
 using namespace netlib;
@@ -65,5 +130,27 @@ struct TimeZone::Data {
     std::vector<std::string> name;
     std::string abbrviation;
 };
+
+TimeZone::TimeZone(const char *zonefile)
+    : _data(new TimeZone::Data)
+{
+}
+
+TimeZone::TimeZone(int eastOfUtc, const char *tzname) 
+    : _data(new TimeZone::Data)
+{
+    _data->localtimes.push_back(netlib::LocalTime(eastOfUtc, false, 0));
+    _data->abbrviation = tzname;
+}
+
+struct tm TimeZone::toLocalTime(time_t seconds) const {
+    struct tm localTime;
+    ::bzero(&localTime, sizeof localTime);
+    assert(_data != NULL);
+    const Data &data(*_data);
+
+    netlib::transition senry(seconds, 0, 0);
+    //const netlib::LocalTime *local = 
+}
 
 
